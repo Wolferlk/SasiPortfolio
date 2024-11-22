70ef8f066ed8ad0495af76fd33438949e25ef89c
wolferlk <sasiofficial25@gmail.com>
2024-11-22 14:32:37 +0530

    data

--- src/pages/ProjectData.tsx
+++ src/pages/ProjectData.tsx
@@ -1,30 +1,32 @@
 const ProjectData = [
     {
       id: 1,
-      title: 'Project One',
-      description: 'A modern web application that demonstrates advanced UI design and backend integration.',
-      image: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97',
-      github: 'https://github.com/yourusername/project-one',
-      live: 'https://project-one.com',
-      technologies: ['React', 'Node.js', 'MongoDB'],
+      title: 'SD MUSIC RECORDS Site',
+      description: 'A modern web Site For Sri lanakan Record Company ',
+      image: 'https://i.ibb.co/gtCFB53/Whats-App-Image-2024-11-22-at-14-02-48-89c88d9c.jpg',
+      github: 'https://github.com/Wolferlk/sdmusicrecords2',
+      live: 'https://sdmusicrecords2.vercel.app/',
+      technologies: ['React', 'Node.js', 'Type Script','Vite'],
       features: [
         'Responsive design with a modern UI',
-        'User authentication and authorization',
+        'Use Platforms embeds (Youtube Spotify)',
         'Integration with third-party APIs',
       ],
-      challenges: 'Faced challenges with API rate limits and resolved them by implementing caching mechanisms.',
+      challenges: '',
       screenshots: [
-        'https://images.unsplash.com/photo-1517694712202-14dd9538aa97',
-        'https://images.unsplash.com/photo-1517694712202-14dd9538aa97',
+        'https://i.ibb.co/gZGdV9F/34-A93-F6-C-5306-42-B0-80-B5-1-DBCB8-BD4-A50.png',
+        'https://i.ibb.co/Ytyb2F1/2-FDEA22-F-DCFC-429-E-AA77-4-B6834-DE4553.png',
+        'https://i.ibb.co/tskHWGQ/FAE211-F2-C87-D-4112-8-FC3-27-D192716-D20.png',
+        'https://i.ibb.co/gtCFB53/Whats-App-Image-2024-11-22-at-14-02-48-89c88d9c.jpg'
       ],
     },
     {
       id: 2,
       title: 'Portfolio Website',
       description: 'A personal portfolio to showcase projects, skills, and professional achievements.',
-      image: 'https://images.unsplash.com/photo-1506765515384-028b60a970df',
-      github: 'https://github.com/yourusername/portfolio-website',
-      live: 'https://portfolio-website.com',
+      image: 'https://i.ibb.co/djjf3s1/244-FEFE5-7-A85-4-DE0-B561-BA25470-A0-CDF.png',
+      github: 'https://github.com/Wolferlk/SasiPortfolio',
+      live: 'https://sasindudiluranga.vercel.app',
       technologies: ['HTML', 'CSS', 'JavaScript'],
       features: [
         'Interactive UI with animations',
@@ -33,8 +35,10 @@ const ProjectData = [
       ],
       challenges: 'Optimized page load speed by implementing lazy loading for images.',
       screenshots: [
-        'https://images.unsplash.com/photo-1506765515384-028b60a970df',
-        'https://images.unsplash.com/photo-1506765515384-028b60a970df',
+        'https://i.ibb.co/djjf3s1/244-FEFE5-7-A85-4-DE0-B561-BA25470-A0-CDF.png',
+        'https://i.ibb.co/bNBgs50/1-E2636-C7-C705-4-EF0-8-E4-B-4-DB00903-B2-C9.png',
+        'https://i.ibb.co/XJ8hxcJ/2-B9-AEF14-3706-4781-9-EE5-96-FE6-D02-F95-A.png',
+        'https://i.ibb.co/t86bVmF/E1562-A32-75-AD-49-AC-8755-451-FF47-E12-BC.png'
       ],
     },
     {
